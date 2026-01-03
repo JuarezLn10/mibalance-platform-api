7b0e531245a5c7ddb975520c42b759087bbea040
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-01-02 22:53:58 -0500

    feat(auth): add queries for retrieving users by id and username

---
+++ src/main/java/com/jrzln/mibalanceapi/auth/domain/model/queries/GetUserByIdQuery.java
@@ -0,0 +1,9 @@
+package com.jrzln.mibalanceapi.auth.domain.model.queries;
+
+/**
+ * @summary
+ * Query to get a user by its id.
+ *
+ * @param id the id of the user.
+ */
+public record GetUserByIdQuery(String id) {}

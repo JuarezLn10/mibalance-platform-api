9a7bd7b576f9e44377c354c852286965ce067132
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-01-02 23:02:50 -0500

    feat(auth): add UserCommandService and UserQueryService interfaces for user authentication and retrieval

---
+++ src/main/java/com/jrzln/mibalanceapi/auth/domain/services/UserCommandService.java
@@ -0,0 +1,33 @@
+package com.jrzln.mibalanceapi.auth.domain.services;
+
+import com.jrzln.mibalanceapi.auth.domain.model.aggregates.User;
+import com.jrzln.mibalanceapi.auth.domain.model.commands.SignInCommand;
+import com.jrzln.mibalanceapi.auth.domain.model.commands.SignUpCommand;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+
+import java.util.Optional;
+
+/**
+ * Service interface for handling user commands such as sign-in and sign-up.
+ * <p>
+ *     This interface defines methods for processing user authentication commands.
+ * </p>
+ */
+public interface UserCommandService {
+
+    /**
+     * Handles the sign-in command.
+     *
+     * @param command the sign-in command containing user credentials
+     * @return an Optional containing an ImmutableTriple with the User, access token, and user id if successful; otherwise, an empty Optional
+     */
+    Optional<ImmutableTriple<User, String, String>> handle(SignInCommand command);
+
+    /**
+     * Handles the sign-up command.
+     *
+     * @param command the sign-up command containing user registration details
+     * @return an Optional containing the created User if successful; otherwise, an empty Optional
+     */
+    Optional<User> handle(SignUpCommand command);
+}

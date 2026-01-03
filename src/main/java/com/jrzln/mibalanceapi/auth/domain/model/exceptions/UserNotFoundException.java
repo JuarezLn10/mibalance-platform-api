c13c65b34dc41688469272a5e8196371458d3b76
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-01-02 22:51:22 -0500

    feat(auth): add UserNotFoundException for handling user retrieval errors

---
+++ src/main/java/com/jrzln/mibalanceapi/auth/domain/model/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.jrzln.mibalanceapi.auth.domain.model.exceptions;
+
+/**
+ * Exception thrown when a user with a specified identifier is not found in the system.
+ */
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(Throwable cause) {
+        super("User with the given identifier was not found:", cause);
+    }
+}

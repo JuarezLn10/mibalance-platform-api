9592f9a5cc1b7263d4d0e0a879a099be26d69ee0
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-01-03 18:26:45 -0500

    feat(auth): add UsersController for user retrieval by ID and email

---
+++ src/main/java/com/jrzln/mibalanceapi/auth/interfaces/rest/controllers/UsersController.java
@@ -0,0 +1,88 @@
+package com.jrzln.mibalanceapi.auth.interfaces.rest.controllers;
+
+import com.jrzln.mibalanceapi.auth.domain.model.aggregates.User;
+import com.jrzln.mibalanceapi.auth.domain.model.queries.GetUserByIdQuery;
+import com.jrzln.mibalanceapi.auth.domain.model.queries.GetUserByUserNameQuery;
+import com.jrzln.mibalanceapi.auth.domain.services.UserQueryService;
+import com.jrzln.mibalanceapi.auth.interfaces.rest.assemblers.UserResourceFromEntityAssembler;
+import com.jrzln.mibalanceapi.auth.interfaces.rest.resources.requests.GetUserByUserNameResource;
+import com.jrzln.mibalanceapi.auth.interfaces.rest.resources.responses.UserResource;
+import com.jrzln.mibalanceapi.shared.domain.model.valueobjects.Email;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * REST controller for user-related endpoints.
+ *
+ * @summary
+ * Handles HTTP requests related to user retrieval.
+ *
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping(value = "/api/v1/users", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Users", description = "Available User Endpoints")
+public class UsersController {
+
+    private final UserQueryService userQueryService;
+
+    public UsersController(UserQueryService userQueryService) {
+        this.userQueryService = userQueryService;
+    }
+
+    /**
+     * Endpoint to get a user by their ID.
+     *
+     * @param userId the ID of the user to retrieve
+     * @return ResponseEntity containing the user resource or a not found status
+     *
+     * @see User
+     */
+    @GetMapping(value = "/{userId}")
+    @Operation(summary = "Get user by id", description = "Get the user with the given id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User retrieved successfully."),
+            @ApiResponse(responseCode = "404", description = "User not found."),
+            @ApiResponse(responseCode = "401", description = "Unauthorized.")})
+    public ResponseEntity<UserResource> getUserById(@PathVariable String userId) {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var user = userQueryService.handle(getUserByIdQuery);
+        if (user.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
+        return ResponseEntity.ok(userResource);
+    }
+
+    /**
+     * Endpoint to get a user by their email.
+     *
+     * @param resource the request resource containing the username (email)
+     * @return ResponseEntity containing the user resource or a not found status
+     *
+     * @see User
+     */
+    @GetMapping
+    @Operation(summary = "Get user by email", description = "Get the user with the given email.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User retrieved successfully."),
+            @ApiResponse(responseCode = "404", description = "User with the given username was not found."),
+            @ApiResponse(responseCode = "401", description = "Unauthorized.")})
+    public ResponseEntity<UserResource> getUserByEmail(@ModelAttribute GetUserByUserNameResource resource) {
+        var email = new Email(resource.username());
+        var getUserByUserNameQuery = new GetUserByUserNameQuery(email);
+        var user = userQueryService.handle(getUserByUserNameQuery);
+        if (user.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
+        return ResponseEntity.ok(userResource);
+    }
+}

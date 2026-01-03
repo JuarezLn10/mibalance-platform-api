2edc7194d9f1803a68534e81ba2d8425ee3cec8e
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-01-02 22:48:19 -0500

    feat(auth): add SignInCommand and SignUpCommand records for user authentication

---
+++ src/main/java/com/jrzln/mibalanceapi/auth/domain/model/commands/SignUpCommand.java
@@ -0,0 +1,12 @@
+package com.jrzln.mibalanceapi.auth.domain.model.commands;
+
+import com.jrzln.mibalanceapi.shared.domain.model.valueobjects.Email;
+
+/**
+ * @summary
+ * Records the data required to sign up a new user.
+ *
+ * @param username the email of the user to be registered
+ * @param password the password of the user to be registered
+ */
+public record SignUpCommand(Email username, String password) {}

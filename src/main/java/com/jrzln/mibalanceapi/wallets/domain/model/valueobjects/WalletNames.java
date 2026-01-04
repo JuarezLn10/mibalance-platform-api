f5091788a9137a2d2023df2d6dba2a3274a8c92e
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-01-03 19:58:18 -0500

    feat(wallets): add WalletNames enum for various digital wallet services

---
+++ src/main/java/com/jrzln/mibalanceapi/wallets/domain/model/valueobjects/WalletNames.java
@@ -0,0 +1,24 @@
+package com.jrzln.mibalanceapi.wallets.domain.model.valueobjects;
+
+/**
+ * Enum representing the names of various digital wallets.
+ * Each constant corresponds to a specific wallet service.
+ */
+public enum WalletNames {
+    YAPE,
+    PLIN,
+    TUNKI,
+    LUKITA,
+    AGORA,
+    BIM,
+    GOOGLE_WALLET,
+    BCP,
+    BBVA,
+    SCOTIABANK,
+    INTERBANK,
+    BANCO_DE_LA_NACION,
+    BANCO_PICHINCHA,
+    COMPARTAMOS,
+    BANCO_FALABELLA,
+    OTHER
+}
